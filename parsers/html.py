39a70ae8b608f60b7a4cc7bdc6e4cec159eef6ca
garrett-vangilder <garrett.vangilder@gmail.com>
2022-11-23 17:46:09 -0600

    lints

--- parsers/html.py
+++ parsers/html.py
@@ -4,10 +4,7 @@ from bs4 import BeautifulSoup
 
 
 class HTMLParser(BaseParser):
-
     def find_match(self, entry: RouteEntry) -> list:
-        # init soup on html
-        soup = BeautifulSoup(entry.html, 'html.parser')
+        soup = BeautifulSoup(entry.html, "html.parser")
 
         return soup.find_all(text=self.regex)
-
