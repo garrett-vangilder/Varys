ba6e804f7b3ba5efcc6e676dac70ecd4f9d6ba78
garrett-vangilder <garrett.vangilder@gmail.com>
2022-11-25 15:25:29 -0600

    reformats

--- parsers/headers.py
+++ parsers/headers.py
@@ -4,6 +4,5 @@ from re import IGNORECASE
 
 
 class HTTPHeaderParser(BaseParser):
-
     def find_match(self, entry: RouteEntry) -> bool:
         return self.regex.findall(str(entry.header), IGNORECASE)
