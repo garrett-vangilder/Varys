81551fcc54ee2fea4b4c4c41a69e9063e0d47a1d
garrett-vangilder <garrett.vangilder@gmail.com>
2022-11-27 17:17:19 -0600

    adds improved logging

--- parsers/__init__.py
+++ parsers/__init__.py
@@ -5,13 +5,16 @@ from domain.objects import RouteEntry
 
 class BaseParser:
     regex = None
+    logger = None
 
-    def __init__(self, regex):
+    def __init__(self, regex, *args, **kwargs):
         if not regex:
             raise ValueError(f"Regex not provided to parser. Instead received: {regex}")
         re_genre = r"{}".format(regex)
 
         self.regex = compile(re_genre)
+
+        self.logger = kwargs.get("logger")
 
     def find_match(self, entry: RouteEntry):
         raise NotImplementedError("Do not use BaseParser")
